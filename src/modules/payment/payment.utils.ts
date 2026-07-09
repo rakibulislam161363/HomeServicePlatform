c5c3c6fba54d2c1c10e45b288ba3c3f1976eaf6f
Rakibul Islam <rakibulislam161363@gmail.com>
2026-07-09 18:21:09 +0600

    payment utils

--- src/modules/payment/payment.utils.ts
+++ src/modules/payment/payment.utils.ts
@@ -1,87 +1,35 @@
-import Stripe from "stripe"
-import { SubscriptionStatus } from "../../../generated/prisma/enums"
-import { prisma } from "../../lib/prisma"
-import { stripe } from "../../lib/stripe"
+import Stripe from "stripe";
+import { prisma } from "../../lib/prisma";
+import { stripe } from "../../lib/stripe";
+import { PaymentStatus, BookingStatus } from "../../../generated/prisma/enums";
 
-export const getPeriodEnd = (payload: Stripe.Subscription) => {
-    const currentPeriodEndInMilliseconds = payload.items.data[0]?.current_period_end!
+export const handleCheckoutCompleted = async (
+  session: Stripe.Checkout.Session
+) => {
+  const bookingId = session.metadata?.bookingId;
 
-    const currentPeriodEnd = new Date(currentPeriodEndInMilliseconds * 1000)
+  if (!bookingId) {
+    console.log("Booking ID not found");
+    return;
+  }
 
-    return currentPeriodEnd
-}
+  await prisma.payment.update({
+    where: {
+      bookingId,
+    },
+    data: {
+      transactionId: session.id,
+      status: PaymentStatus.SUCCESS,
+      paidAt: new Date(),
+    },
+  });
 
-export const handleCheckoutCompleted = async (session: Stripe.Checkout.Session) => {
-    const userId = session.metadata?.userId
-    const stripeCustomerId = session.customer as string
-    const stripeSubscriptionId = session.subscription as string;
-
-    if (!userId || !stripeSubscriptionId || !stripeCustomerId) {
-        console.log("Webhook : Missing values For Creating Checkout Session");
-        return;
-    }
-
-    const stripeSubscription = await stripe.subscriptions.retrieve(stripeSubscriptionId);
-
-
-    const currentPeriodEnd = getPeriodEnd(stripeSubscription)
-
-
-    await prisma.subscription.upsert({
-        where: {
-            userId
-        },
-
-        create: {
-            userId,
-            stripeCustomerId,
-            stripeSubscriptionId,
-            status: "ACTIVE",
-            currentPeriodEnd,
-        },
-
-        update: {
-            stripeCustomerId,
-            stripeSubscriptionId,
-            status: "ACTIVE",
-            currentPeriodEnd,
-        }
-
-    })
-
-}
-
-export const handleChangeSubscription = async (payload: Stripe.Subscription) => {
-
-    const stripeSubscriptionId = payload.id;
-
-    const status =
-        (payload.status === "active" || payload.status === "trialing") ? SubscriptionStatus.ACTIVE :
-            payload.status === "canceled" ? SubscriptionStatus.CANCELED :
-                SubscriptionStatus.EXPIRED
-
-    const currentPeriodEnd = getPeriodEnd(payload)
-
-    const isSubscriptionExist = await prisma.subscription.findUnique({
-        where: {
-            stripeSubscriptionId
-        }
-    })
-
-    if (!isSubscriptionExist) {
-        console.log(`Webhook : No Subscription found for subscription id : ${stripeSubscriptionId}`);
-
-        return;
-    }
-
-    await prisma.subscription.update({
-        where: {
-            stripeSubscriptionId
-        },
-        data: {
-            status,
-            currentPeriodEnd
-        }
-    })
-
-}
+  await prisma.booking.update({
+    where: {
+      id: bookingId,
+    },
+    data: {
+      status: BookingStatus.PAID,
+    },
+  });
+};
